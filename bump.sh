7d3c0edb81a53e63ffc44e2f91bedbef2e20c34f
Gijsjan <agijsbro@gmail.com>
2017-12-11 12:39:14 +0100

    Add bump script

---
+++ bump.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ "$1" != "patch" ] && [ "$1" != "minor" ] && [ "$1" != "major" ]; then
+	echo "Error. Parameter should be patch | minor | major."
+	exit 1
+fi
+
+rm -rf build
+npm run build
+if [ $? -ne 0 ]; then exit 1; fi
+
+npm version $1 -m "v%s tagged"
+npm publish
+
+git push && git push --tags
