49fa90133155ff9f7285a22551a73060911f3687
Gijsjan <agijsbro@gmail.com>
2017-12-12 11:18:51 +0100

    Don't use absolute path to pergamon-ui-components deps

--- build/create-tree.d.ts
+++
@@ -1,5 +0,0 @@
-import Annotation from 'pergamon-ui-components/build/models/annotation';
-import TreeNode from 'pergamon-ui-components/build/models/tree-node';
-export declare const generateNodeId: (node: TreeNode, withSuffix?: boolean) => string;
-declare const createTree: (root: Annotation) => TreeNode[];
-export default createTree;
